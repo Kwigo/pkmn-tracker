ae8658390d35dd8b6f378378891d73fa4629fdc8
Kwigo <82644313+Kwigo@users.noreply.github.com>
2026-04-10 09:12:24 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
-console.log("MAIN JS IS LOADING");
+//async loading of json data
 async function loadGen1() {
-  console.log("loading gen1...");
+  //console.log("loading gen1...");
 
   const res = await fetch("assets/data/gen1.json");
-  console.log("response:", res);
+  //console.log("response:", res);
 
   const data = await res.json();
-  console.log("data:", data);
+  //console.log("data:", data);
+
+  const bulbasaur = data.pokemon[0];
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+  <h3>${bulbasaur.name}</h3>
+  <p>#${bulbasaur.id}</p>
+  `;
+document.getElementById("app").appendChild(card);
 }
 
 loadGen1();
