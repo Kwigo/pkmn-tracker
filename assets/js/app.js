a72da7d52de321e93f4a6cfe79d8f6a101e4b343
Kwigo <82644313+Kwigo@users.noreply.github.com>
2026-04-12 14:14:42 +0200

    fix js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
 import { loadAllGens } from "./data.js";
+import { state } from "./state.js";
+import { gens } from "./data.js";
+import { createGenSection } from "./ui.js";
 import { renderGen } from "./render.js";
 
-loadAllGens(renderGen);
+loadAllGens();
+createGenSection(gens, state.data);
+
--- assets/js/data.js
+++ assets/js/data.js
@@ -3,16 +3,13 @@ import { createGenSection } from "./ui.js";
 
 export const gens = [1,2,3,4,5,6,7,8,9,0];
 
-export async function loadAllGens(section) {
+export async function loadAllGens() {
   for (const gen of gens) {
     const res = await fetch(`assets/data/gen${gen}.json`);
     const data = await res.json();
 
     state.data[gen] = data;
 
-    createGenSection(gen, data, document.getElementById("gens-container"));
-    console.log("GEN DATA:", data);
-    console.log("POKEMON:", data.pokemon);
-    console.log("IS ARRAY?", Array.isArray(data.pokemon));
+    createGenSection(gen, data);
   }
 }
